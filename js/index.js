bd9a70ee905554d7329a3cf767445e03a97cb811
경아 <sun4354@naver.com>
2025-04-24 15:58:09 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -54,9 +54,86 @@ function onScroll() {
 
 	$('.link_page').removeClass('active');
 	$('.link_page').eq(i).addClass('active');
+	$('.fix-wrap .link').eq(i).addClass('active');
 }
 
 $(window).scroll(onScroll).trigger('scroll');
+
+/* 모바일 윙*/
+function fixShow(show) {
+	if (show) {
+		$('.fix-wrap').css('display', 'block');
+		setTimeout(function () {
+			$('.fix-wrap').addClass('active');
+			$('.fix-wrap > div').addClass('active');
+		}, 0);
+	} else {
+		$('.fix-wrap').removeClass('active');
+		$('.fix-wrap > div').removeClass('active');
+		setTimeout(function () {
+			$('.fix-wrap').css('display', 'none');
+		}, 100);
+	}
+}
+
+function onBarClick() {
+	if ($(this).hasClass('default')) {
+		$(this).removeClass('default').addClass('active');
+		fixShow(true);
+	} else if ($(this).hasClass('active')) {
+		$(this).removeClass('active').addClass('default');
+		fixShow(false);
+	} else {
+		$(this).addClass('active');
+		fixShow(true);
+	}
+}
+
+$('.navi-bars').click(onBarClick);
+
+/* 모바일 윙 숨기기 */
+function onResize() {
+	if ($(this).innerWidth() > 1200) {
+		$('.navi-bars').removeClass('active');
+		$('.fix-wrap > div').removeClass('active');
+		$('.fix-wrap').removeClass('active');
+		$('.fix-wrap').hide();
+	}
+
+	initAllSlides();
+}
+
+$(window).resize(onResize);
+$(document).ready(() => {
+	initAllSlides();
+	$(window).trigger('resize');
+});
+
+/* 모바일 슬라이드 이미지 스크롤 센터 */
+$('.page3 .slide_img2, .page3 .slide_img3, .page3 .slide_img4').each(function () {
+	let $el = $(this);
+	let scrollMiddle = ($el[0].scrollWidth - $el.innerWidth()) / 2;
+	$el.scrollLeft(scrollMiddle);
+});
+
+/* 모바일 메뉴 클릭 숨기기*/
+$('.fix-wrap .link').click(function () {
+	setTimeout(() => {
+		$('.navi-bars').removeClass('active').addClass('default');
+		fixShow(false);
+	}, 100);
+});
+
+/* fix-wrap */
+$('.fix-wrap').click(function (e) {
+	$('.navi-bars').removeClass('active').addClass('default');
+	fixShow(false);
+	wrapperSwipers.forEach(swiper => swiper.autoplay.start());
+});
+
+$('.pop, .fix-wrap > div').click(function (e) {
+	e.stopPropagation();
+});
 
 /* 광고 집행 문의 */
 $('.contact_link').click(function () {
@@ -134,12 +211,15 @@ function handlePage4Wheel(e) {
 }
 
 /* 0 */
+let swiper0;
+
 function initSlide0() {
+
 	swiper0 = new Swiper('.swiper.type0', {
 		effect: 'slide',
 		direction: 'horizontal',
 		slidesPerView: 'auto',
-		spaceBetween: 50,
+		spaceBetween: 20,
 		observer: true,
 		observeParents: true,
 		loop: true,
@@ -152,6 +232,11 @@ function initSlide0() {
 			disableOnInteraction: false,
 		},
 		allowTouchMove: false,
+		breakpoints: {
+			1200: {
+				spaceBetween: 50
+			}
+		}
 	});
 }
 
@@ -184,6 +269,12 @@ const swiper1 = new Swiper('.swiper.type1', {
 			return `<span class="${className}" data-index="${index}">${customLabels[index] || index + 1}</span>`;
 		},
 	},
+	allowTouchMove: false,
+	breakpoints: {
+		890: {
+			allowTouchMove: true,
+		}
+	},
 	on: {
 		init: function () {
 			updatePagination(this);
@@ -211,9 +302,6 @@ let swiper2;
 function initSlide2() {
 	swiper2 = new Swiper('.swiper.type2', {
 		loop: false,
-		/* autoplay: {
-			delay: 5000,
-		}, */
 		speed: 1000,
 		spaceBetween: 30,
 		pagination: {
@@ -263,6 +351,12 @@ const swiper4 = new Swiper('.swiper.type4', {
 		el: '.swiper-pagination.type4',
 		clickable: true,
 	},
+	allowTouchMove: false,
+	breakpoints: {
+		960: {
+			allowTouchMove: true,
+		}
+	},
 	on: {
 		slideChangeTransitionEnd: function () {
 			if (this.realIndex === 0) {
@@ -277,44 +371,92 @@ const swiper4 = new Swiper('.swiper.type4', {
 	}
 });
 
-/* 5 */
-const swiper5 = new Swiper('.swiper.type5', {
+/* 5 ~ 11 */
+let swipers = {};
+const swiperConfigs = [{
+		selector: '.swiper.type5',
+		spaceBetween: 50
+	},
+	{
+		selector: '.swiper.type6',
+		spaceBetween: 50
+	},
+	{
+		selector: '.swiper.type7',
+		spaceBetween: 30
+	},
+	{
+		selector: '.swiper.type8',
+		spaceBetween: 30
+	},
+	{
+		selector: '.swiper.type9',
+		spaceBetween: 30
+	},
+	{
+		selector: '.swiper.type10',
+		spaceBetween: 30
+	},
+	{
+		selector: '.swiper.type11',
+		spaceBetween: 30
+	},
+];
+
+function initAllSlides() {
+	swiperConfigs.forEach((config, index) => {
+		const key = `swiper${index + 5}`;
+
+		if (swipers[key]) {
+			swipers[key].destroy(true, true);
+		}
+
+		swipers[key] = new Swiper(config.selector, {
+			effect: 'slide',
+			direction: 'horizontal',
+			slidesPerView: 'auto',
+			spaceBetween: config.spaceBetween,
+			observer: true,
+			observeParents: true,
+			loop: true,
+			initialSlide: 0,
+			centeredSlidesBounds: true,
+			speed: 3000,
+			autoplay: {
+				delay: 0,
+				disableOnInteraction: false,
+			},
+		});
+	});
+}
+
+/* 12 */
+const swiper12 = new Swiper('.swiper.type12', {
 	effect: 'slide',
-	direction: 'horizontal',
 	slidesPerView: 'auto',
-	spaceBetween: 50,
-	observer: true,
-	observeParents: true,
-	loop: true,
-	initialSlide: 0,
-	centeredSlidesBounds: true,
-	speed: 2000,
-	autoplay: {
-		delay: 0,
-		disableOnInteraction: false,
+	spaceBetween: 20,
+	loop: false,
+	breakpoints: {
+		1200: {
+			spaceBetween: 40,
+		}
 	},
 });
 
-/* 6 */
-const swiper6 = new Swiper('.swiper.type6', {
-	effect: 'slide',
-	direction: 'horizontal',
-	slidesPerView: 'auto',
-	spaceBetween: 50,
-	observer: true,
-	observeParents: true,
-	loop: true,
-	initialSlide: 0,
-	centeredSlidesBounds: true,
-	speed: 2000,
-	autoplay: {
-		delay: 0,
-		disableOnInteraction: false,
-	},
-});
+function updatePagination(swiperInstance) {
+	const bullets = swiperInstance.pagination.bullets;
+	bullets.forEach((bullet, index) => {
+		if (bullet.classList.contains('swiper-pagination-bullet-active')) {
+			bullet.innerHTML = customLabelsActive[index] || (index + 1);
+		} else {
+			bullet.innerHTML = customLabels[index] || (index + 1);
+		}
+	});
+}
 
-/***** 그래프 initCharts /*****/
 function initCharts() {
+	const getScaleFactor = (ctx) => ctx.canvas.width / 390;
+
 	const ctx1 = document.getElementById('myChart1').getContext('2d');
 	myChart1 = new Chart(ctx1, {
 		type: 'bar',
@@ -340,11 +482,11 @@ function initCharts() {
 			scales: {
 				xAxes: [{
 					gridLines: {
-						display: false,
+						display: false
 					},
 					ticks: {
 						fontColor: '#777',
-						fontSize: 18,
+						/* fontSize: 18, */
 						fontFamily: 'Pretendard',
 					},
 					barPercentage: 0.8,
@@ -355,7 +497,7 @@ function initCharts() {
 						drawBorder: false
 					},
 					ticks: {
-						display: false,
+						display: false
 					}
 				}]
 			},
@@ -363,6 +505,7 @@ function initCharts() {
 				onComplete: function () {
 					const chart = this.chart;
 					const ctx = chart.ctx;
+					const scaleFactor = getScaleFactor(ctx);
 					ctx.textAlign = 'center';
 					ctx.textBaseline = 'bottom';
 
@@ -374,12 +517,13 @@ function initCharts() {
 							const centerX = bar._model.base + barWidth;
 							const centerY = bar._model.y - 10;
 							const labelText = `${value}만`;
+
 							if (index === 5) {
 								ctx.fillStyle = '#0d6eee';
-								ctx.font = '600 23px Pretendard';
+								ctx.font = `600 ${Math.round(23 * scaleFactor)}px Pretendard`;
 							} else {
 								ctx.fillStyle = '#bbb';
-								ctx.font = '500 16px Pretendard';
+								ctx.font = `500 ${Math.round(16 * scaleFactor)}px Pretendard`;
 							}
 
 							ctx.fillText(labelText, centerX, centerY);
@@ -418,18 +562,19 @@ function initCharts() {
 					const meta = chart.getDatasetMeta(0);
 					const dataset = this.data.datasets[0].data;
 					const total = dataset.reduce((a, b) => a + b, 0);
+					const scaleFactor = getScaleFactor(ctx);
 
 					const fontStyles = [{
 							color: '#777',
-							size: 16
+							size: Math.round(16 * scaleFactor)
 						},
 						{
 							color: '#fff',
-							size: 16
+							size: Math.round(16 * scaleFactor)
 						},
 						{
 							color: '#fff',
-							size: 20
+							size: Math.round(20 * scaleFactor)
 						}
 					];
 
@@ -485,7 +630,7 @@ function initCharts() {
 						drawBorder: false
 					},
 					ticks: {
-						display: false,
+						display: false
 					}
 				}],
 				yAxes: [{
@@ -496,7 +641,7 @@ function initCharts() {
 						drawBorder: false
 					},
 					ticks: {
-						display: false,
+						display: false
 					}
 				}]
 			},
@@ -504,8 +649,9 @@ function initCharts() {
 				onComplete: function () {
 					const chart = this.chart;
 					const ctx = chart.ctx;
+					const scaleFactor = getScaleFactor(ctx);
 
-					ctx.font = '500 18px Pretendard';
+					ctx.font = `500 ${Math.round(18 * scaleFactor)}px Pretendard`;
 					ctx.textAlign = 'center';
 					ctx.textBaseline = 'middle';
 
